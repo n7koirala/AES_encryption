a17788b36b574a9b3ee300aaf6e6430a1bace50e
NIRAJAN KOIRALA <31283818+nirajank07@users.noreply.github.com>
2020-11-14 18:24:48 -0500

    Create CTR_script.py

---
+++ CTR/CTR_script.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+
+from Crypto import Random
+from Crypto.Cipher import AES
+import os
+import os.path
+from os import listdir
+from os.path import isfile, join
+import time
+from base64 import b64decode
+from Crypto.Util.Padding import pad,unpad
+import pyaes
+
+
+class Encryptor:
+    def __init__(self, key):
+        self.key = key
+
+
+    def encrypt(self, message, key):
+        AES_obj = pyaes.AESModeOfOperationCTR(key)
+        ciphertext = AES_obj.encrypt(message)
+        return ciphertext
+
+    def encrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            plaintext = fo.read()
+        enc = self.encrypt(plaintext, self.key)
+        with open(file_name + ".enc", 'wb') as fo:
+            fo.write(enc)
+        os.remove(file_name)
+
+    def decrypt(self, ciphertext, key):
+        AES_obj = pyaes.AESModeOfOperationCTR(key)
+        decrypted = AES_obj.decrypt(ciphertext)
+        return decrypted
+
+    def decrypt_file(self, file_name):
+        with open(file_name, 'rb') as fo:
+            ciphertext = fo.read()
+        dec = self.decrypt(ciphertext, self.key)
+        with open(file_name[:-4], 'wb') as fo:
+            fo.write(dec)
+        os.remove(file_name)
+
+    def getAllFiles(self):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        dirs = []
+        for dirName, subdirList, fileList in os.walk(dir_path):
+            for fname in fileList:
+                if (fname != 'script.py'):
+                    dirs.append(dirName + "//" + fname)
+        return dirs
+
+    def encrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.encrypt_file(file_name)
+
+    def decrypt_all_files(self):
+        dirs = self.getAllFiles()
+        for file_name in dirs:
+            self.decrypt_file(file_name)
+
+
+key = pad(b"myKey", AES.block_size)
+enc = Encryptor(key, iv)
+clear = lambda: os.system('cls')
+
+def main():
+    while True:
+        clear()
+        choice = int(input(
+            " Select an option: \n1 -> Encrypt single file\n2 -> Decrypt single file\n3 -> Encrypt all files in the curr folder\n4 -> Decrypt all files in the curr folder\n5 -> Quit\n"))
+        clear()
+        if choice == 1:
+            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
+        elif choice == 2:
+            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
+        elif choice == 3:
+            start = time.time()
+            enc.encrypt_all_files()
+            end = time.time()
+            print("Took " + str(end - start) + " seconds to encrypt!")
+        elif choice == 4:
+            start = time.time()
+            enc.decrypt_all_files()
+            end = time.time()
+            print("Took " + str(end - start) + " seconds to decrypt!")
+        elif choice == 5:
+            exit()
+        else:
+            print("Please select a valid option!")
+
+if __name__ == "__main__":
+    main()
